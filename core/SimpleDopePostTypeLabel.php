55379686725bb38b69b2ad490617404cff0bb002
glockenmeier <d.glockenmeier@gmail.com>
2012-11-21 21:35:18 +0100

    removed conflicting variables

--- core/SimpleDopePostTypeLabel.php
+++ core/SimpleDopePostTypeLabel.php
@@ -11,36 +11,36 @@
  * @package core
  */
 final class SimpleDopePostTypeLabel extends DopePostTypeLabel {
-    private $singular;
-    private $plural;
+
     private $object = null;
-    
+
     public function __construct($singular, $plural) {
         parent::__construct($singular, $plural);
     }
-    
+
     /**
      * Returns as post type label object as expected by register_post_type args.
      * @return array label array
      */
-    public function getLabelObject(){
+    public function getLabelObject() {
         if ($this->object === null) {
             $this->createLabelObject();
         }
         return $this->object;
     }
-    
+
     private function createLabelObject() {
         $this->object = array(
-        'name' => $this->plural,
-        'singular_name' => $this->singular,
-        'add_new_item' => sprintf(__('Add New %s', 'dope-label'), $this->singular),
-        'edit_item' => sprintf(__('Edit %s', 'dope-label'), $this->singular),
-        'new_item' => sprintf(__('Add %s', 'dope-label'),$this->singular),
-        'view_item' => sprintf(__('View %s', 'dope-label'), $this->singular),
-        'search_items' => sprintf(__('Search %s', 'dope-label'), $this->plural),
-        'not_found' => sprintf(__('No %s found', 'dope-label'), $this->plural),
-        'not_found_in_trash' => sprintf(__('No %s found in trash', 'dope-label'), $this->plural)
+            'name' => $this->plural,
+            'singular_name' => $this->singular,
+            'add_new_item' => sprintf(__('Add New %s', 'dope-label'), $this->singular),
+            'edit_item' => sprintf(__('Edit %s', 'dope-label'), $this->singular),
+            'new_item' => sprintf(__('Add %s', 'dope-label'), $this->singular),
+            'view_item' => sprintf(__('View %s', 'dope-label'), $this->singular),
+            'search_items' => sprintf(__('Search %s', 'dope-label'), $this->plural),
+            'not_found' => sprintf(__('No %s found', 'dope-label'), $this->plural),
+            'not_found_in_trash' => sprintf(__('No %s found in trash', 'dope-label'), $this->plural)
         );
     }
+
 }
